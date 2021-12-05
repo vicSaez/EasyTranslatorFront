ac0051fd25c1eb7920aabcd37079cd41ce09e2c1
Vic Saez <vhsaez@gmail.com>
2021-12-05 20:29:08 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,120 @@
-import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Button, Paper, TextField,FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
-function App() {
+function CallAPI(sourceLanguage,targetLanguage,sourceText) {
+
+  return fetch('https://localhost:49155/api/v1/Translate?sourceLanguage='+sourceLanguage+'&targetLanguage='+targetLanguage+'&sourceText='+sourceText, { method: 'GET', mode: 'cors' })
+  .then(data => data.json());
+
+}
+
+export default function App() {
+
+  const [sourceText, setSourceText] = React.useState('');
+  const [targetText, setTargetText] = React.useState('');
+  const [sourceLanguage, setSourceLanguage] = React.useState('auto');
+  const [targetLanguage, setTargetLanguage] = React.useState('de');
+  
+  const handleSorceTextChange = (event) => {
+    setSourceText(event.target.value);
+  };
+
+  const handleSourceLanguageChange = (event) => {
+    setSourceLanguage(event.target.value);
+  };
+
+  const handleTargetLanguageChange = (event) => {
+    setTargetLanguage(event.target.value);
+  };
+
+  const translateClick = () => {
+    CallAPI(sourceLanguage,targetLanguage,sourceText).then( (json) => {setTargetText(json.targetText);}); //JSON.stringify(json)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="sm" className="App">
+      <Box >
+      <Typography variant="h4" component="h1" gutterBottom align="left">
+          Easy Translator V1
+        </Typography>
+      </Box>
+      <Box>
+        <Paper>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Source Text"
+          multiline
+          maxRows={4}
+          value={sourceText}
+          onChange={handleSorceTextChange}
+          fullWidth
+        />
+        </Paper>
+      </Box>
+      <Box sx={{ display: 'flex', flexDirection: 'row', p:2 }}>
+        <Box sx={{ minWidth:120 }}>
+        <FormControl >
+        <InputLabel id="demo-simple-select-label">Source</InputLabel> 
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={sourceLanguage}
+              label="Source"
+              onChange={handleSourceLanguageChange}
+             >
+              <MenuItem value={"auto"}>Auto</MenuItem>
+              <MenuItem value={"en"}>English</MenuItem>
+              <MenuItem value={"de"}>German</MenuItem>
+              <MenuItem value={"fr"}>French</MenuItem>
+            </Select>
+        </FormControl>
+        </Box>
+        <Box>
+        <FormControl >
+          <InputLabel id="demo-simple-select-label">Target</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={targetLanguage}
+              label="Target"
+              onChange={handleTargetLanguageChange}
+             >
+              <MenuItem value={"en"}>English</MenuItem>
+              <MenuItem value={"de"}>German</MenuItem>
+              <MenuItem value={"fr"}>French</MenuItem>
+            </Select>
+        </FormControl>
+        </Box>
+        <Box sx={{p:1}}>
+          <Button variant="contained" color="primary" onClick={translateClick}>
+          Translate
+          </Button>
+        </Box>
+      </Box>
+      <Box>
+        <Paper>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Tanslation"
+          multiline
+          maxRows={4}
+          value={targetText}
+          fullWidth
+          InputProps={{
+            readOnly: true,
+          }}
+        />
+        </Paper>
+      </Box>
+    </Container>
   );
 }
 
-export default App;
+
+
+
